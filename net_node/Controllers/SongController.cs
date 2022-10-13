3e102434dab55857f2a07c68d14505aaf3489410
Aron <aronchocama@gmail.com>
2022-10-13 13:45:06 -0500

    actualzacionAgregar archivos de proyecto.

---
+++ net_node/Controllers/SongController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using net_node.Models;
+using net_node.Repositories;
+
+namespace net_node.Controllers
+{
+    public class SongController : Controller
+    {
+        private IAlbumCollection db = new AlbumCollection();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SongController/Create
+        public ActionResult Create(string id)
+        {
+            SongViewModel songVM = new SongViewModel() {AlbumId = id, Song = new Song() };
+            return View(songVM);
+        }
+
+        // POST: SongController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                var album = db.GetAlbumId(collection["AlbumId"]);
+                if (album.Songs == null)
+                    album.Songs = new List<Song>();
+                album.Songs.Add(new Song
+                {
+                    Name = collection["Song.Name"],
+                    Duration = int.Parse(collection["Song.Duration"])
+                });
+                db.UpdateAlbum(album);
+                return RedirectToAction("index","Album");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ net_node/Repositories/AlbumCollection.cs
@@ -0,0 +1,51 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using net_node.Models;
+
+namespace net_node.Repositories
+{
+    public class AlbumCollection : IAlbumCollection
+    {
+        internal MongoDBRepository _repository = new MongoDBRepository();
+        private IMongoCollection<Album> Collection;
+
+        public AlbumCollection()
+        {
+            Collection = _repository.db.GetCollection<Album>("Albums");
+        }
+
+        public void DeleteAlbum(string id)
+        {
+            var filter = Builders<Album>.Filter.Eq(s => s.Id, new ObjectId(id));
+            Collection.DeleteOneAsync(filter);
+        }
+
+        public Album GetAlbumId(string id)
+        {
+            var album = Collection.Find(
+                new BsonDocument{{ "_id", new ObjectId(id)}}).FirstAsync().Result;
+            return album;
+        }
+
+        public List<Album> GetAllAlbum()
+        {
+            var query = Collection.
+                Find(new BsonDocument()).ToListAsync();
+            return query.Result;
+        }
+
+        public void InsertAlbum(Album album)
+        {
+            Collection.InsertOneAsync(album);
+        }
+
+        public void UpdateAlbum(Album album)
+        {
+            var filter = Builders<Album>
+                .Filter
+                .Eq(s => s.Id, album.Id );
+
+            Collection.ReplaceOneAsync(filter, album);
+        }
+    }
+}
